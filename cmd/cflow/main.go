a78aae49dfe14f7ef7cd2da8900a7fb3f0a959d0
Julio Milan <271515490+mlloc@users.noreply.github.com>
2026-08-18 11:40:55 -0300

    add rpc client

--- cmd/cflow/main.go
+++ cmd/cflow/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/mlloc/cflow/internal/btc"
+	"github.com/mlloc/cflow/internal/env"
+)
+
+func main() {
+	vars := env.Load()
+
+	_, err := btc.NewClient(vars)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/btc/btc.go
+++ internal/btc/btc.go
@@ -2,9 +2,28 @@ package btc
 
 import (
 	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/mlloc/cflow/internal/env"
 )
 
 type Client struct {
 	rpc *rpcclient.Client
 }
 
+func NewClient(env env.Env) (*Client, error) {
+	config := &rpcclient.ConnConfig{
+		Host: env.BTC.Host,
+		User: env.BTC.User,
+		Pass: env.BTC.Pass,
+		HTTPPostMode: true,
+		DisableTLS: true,
+	}
+
+	rpc, err := rpcclient.New(config, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		rpc: rpc,
+	}, nil
+}
